862ab151643dd8a65a3ba4988ce46599515c5d9f
Matthew Lou-Magnuson <mloumagnuson@gmail.com>
2016-08-31 20:18:13 +0800

    improved timing

---
+++ edx-srv/public/agl/logic.js
@@ -0,0 +1,204 @@
+var fwd_train = [ "G", "Z", "A", "X", "D", "Y", "G", "Z", "B", "X", "F", "Y", "D", "Y", "A", "X", "Z", "G", "E", "Y", "D", "Y", "F", "Y", "A", "X", "I", "Z", "B", "X", "E", "Y", "D", "Y", "H", "Z", "G", "Z", "C", "X", "D", "Y", "A", "X", "G", "Z", "G", "Z", "A", "X", "F", "Y", "D", "Y", "I", "Z", "D", "Y", "H", "Z", "G", "Z", "D", "Y", "A", "X", "G", "Z", "I", "Z", "A", "X", "A", "X", "E", "Y", "D", "Y", "G", "Z", "D", "Y", "A", "X", "G", "Z", "B", "X", "G", "Z", "I", "Z", "A", "X", "G", "Z", "I", "Z", "A", "X", "E", "Y", "D", "Y", "C", "X", "A", "X", "D", "Y", "B", "X", "G", "Z", "B", "X", "E", "Y", "E", "Y", "D", "Y", "F", "Y", "G", "Z", "D", "Y", "I", "Z", "A", "X", "D", "Y", "A", "X", "B", "X", "G", "Z", "H", "Z", "I", "Z", "D", "Y", "I", "Z", "B", "X", "F", "Y", "D", "Y", "H", "Z", "C", "X", "D", "Y", "A", "X", "F", "Y", "H", "Z", "A", "X", "D", "Y", "G", "Z", "A", "X", "B", "X", "G", "Z", "E", "Y", "G", "Z", "A", "X", "D", "Y", "I", "Z", "A", "X", "H", "Z", "C", "X", "A", "X", "D", "Y", "A", "X", "G", "Z", "A", "X", "E", "Y", "F", "Y", "G", "Z", "A", "X", "B", "X", "G", "Z", "D", "Y", "B", "X", "E", "Y", "A", "X", "D", "Y", "A", "X", "G", "Z", "F", "Y", "C", "X", "A", "X", "G", "Z", "D", "Y", "A", "X", "D", "Y", "G", "Z", "D", "Y", "A", "X", "D", "Y", "A", "X", "G", "Z", "A", "X", "C", "X", "A", "X", "D", "Y", "G", "Z", "D", "Y", "A", "X", "F", "Y", "E", "Y", "A", "X", "C", "X", "A", "X", "G", "Z", "A", "X", "D", "Y", "A", "X", "I", "Z", "A", "X", "D", "Y", "A", "X", "G", "Z", "A", "X", "D", "Y", "E", "Y", "I", "Z", "E", "Y", "G", "Z", "A", "X", "B", "X", "G", "Z", "D", "Y", "A", "X", "E", "Y", "A", "X", "G", "Z", "B", "X", "D", "Y", "G", "Z", "I", "Z", "G", "Z", "D", "Y", "A", "X", "D", "Y", "A", "X", "H", "Z", "G", "Z", "A", "X", "G", "Z", "A", "X", "G", "Z", "D", "Y", "E", "Y", "D", "Y", "G", "Z", "I", "Z", "C", "X", "B", "X", "D", "Y", "H", "Z", "C", "X", "H", "Z", "G", "Z", "B", "C", "X", "X", "A", "X", "E", "Y", "A", "X", "D", "Y", "H", "Z", "D", "Y", "C", "X", "H", "Z", "D", "Y", "H", "Z", "G", "Z", "A", "X", "G", "Z", "A", "X", "I", "Z", "E", "Y", "D", "Y", "H", "Z", "B", "X", "A", "X", "G", "Z", "A", "X", "C", "X", "A", "X", "G", "Z", "E", "Y", "A", "X", "G", "Z", "F", "Y", "D", "Y", "G", "Z", "C", "X", "B", "X", "F", "Y", "G", "Z", "A", "X", "F", "Y", "H", "Z", "F", "Y", "G", "Z", "B", "X", "C", "X", "A", "X", "F", "Y", "D", "Y", "A", "X", "C", "X", "A", "X", "B", "X", "I", "Z", "A", "X", "D", "Y", "A", "X", "F", "Y", "D", "Y", "A", "X", "I", "Z", "A", "X", "H", "Z", "A", "X", "H", "Z", "G", "Z", "A", "X", "F", "Y", "D", "Y", "C", "X", "G", "Z", "C", "X", "B", "X", "A", "X", "D", "Y", "F", "Y", "G", "Z", "B", "X", "C", "X", "B", "X", "C", "X", "H", "Z", "F", "Y", "I", "Z", "A", "X", "G", "Z", "A", "X", "G", "Z", "F", "Y", "G", "Z", "H", "Z", "G", "Z", "I", "Z", "G", "Z", "F", "Y", "D", "Y", "A", "X", "D", "Y", "C", "X", "G", "Z", "B", "X", "G", "Z", "F", "Y", "C", "X", "B", "X", "E", "Y", "G", "Z", "A", "X", "G", "Z", "A", "X", "I", "Z", "H", "Z", "A", "X", "G", "Z", "A", "X", "G", "Z", "F", "Y", "G", "Z", "E", "Y", "D", "Y", "A", "X", "H", "Z", "G", "Z", "A", "X", "G", "Z", "D", "Y", "A", "X", "G", "Z", "B", "X", "G", "Z", "A", "X", "G", "Z", "D", "Y", "A", "X", "G", "Z", "B", "X", "D", "Y", "C", "X", "F", "Y", "G", "Z", "E", "Y", "I", "Z", "D", "Y", "F", "Y", "B", "X", "D", "Y", "I", "Z", "E", "Y", "A", "X", "D", "Y", "G", "Z", "E", "Y", "G", "Z", "D", "Y", "A", "X", "H", "Z", "D", "Y", "A", "X", "H", "Z", "A", "X", "G", "Z", "A", "X", "I", "Z", "D", "Y", "A", "X", "E", "Y", "D", "Y" ]
+
+var fwd_test = [ ["X","A","C","X"], ["X","A","B","X"], ["X","A","E","Y"], ["X","D","B","X"], ["X","D","C","X"], ["X","D","E","Y"], ["Y","D","B","X"], ["Y","D","C","X"], ["Y","D","E","Y"], ["Y","G","B","X"], ["Y","G","C","X"], ["Y","G","E","Y"], ["Z","A","B","X"], ["Z","A","C","X"], ["Z","A","E","Y"], ["Z","G","B","X"], ["Z","G","C","X"], ["Z","G","E","Y"], ["B","X","X","A"], ["B","X","X","D"], ["B","X","Y","D"], ["C","X","X","A"], ["C","X","X","D"], ["C","X","Y","D"], ["E","Y","X","A"], ["E","Y","X","D"], ["E","Y","Y","D"], ["F","Y","X","A"], ["F","Y","X","D"], ["F","Y","Y","D"], ["H","Z","X","A"], ["H","Z","X","D"], ["H","Z","Y","D"], ["I","Z","X","A"], ["I","Z","X","D"], ["I","Z","Y","D"] ]
+
+var Q //load queue
+var canvas
+var stage
+var stims
+var mapsym
+var stim_start
+var pause_start
+var dataSet = []
+var stim_index = 0
+var test_index = 0
+var needPause = 1
+var train_end_index = fwd_train.length
+var test_end_index = fwd_test.length
+var stim_delay = 500
+var test_pause_delay = 1200
+var train_pause_delay = 250
+var mode = 'none'
+var font_info = "30px Times"
+
+//TEST SWITCHES/
+//stim_index = train_end_index
+
+function getMS() {
+  time = window.performance.now()
+  return time
+}
+
+function randomize(array){
+    var i = array.length, j, temp;
+    while ( --i ){
+        j = Math.floor( Math.random() * (i - 1) );
+        temp = array[i]
+        array[i] = array[j]
+        array[j] = temp;
+    }
+}
+
+function init(){
+    canvas = document.getElementById('canvas')
+    stage = new createjs.Stage(canvas)
+    preload()
+}
+
+function preload(){
+  Q = new createjs.LoadQueue()
+  Q.installPlugin(createjs.Sound)
+  Q.on("complete", genStims, this)
+  Q.loadManifest([
+     {id: "s1", src:"./images/Slide01.jpg"},
+     {id: "s2", src:"./images/Slide02.jpg"},
+     {id: "s3", src:"./images/Slide03.jpg"},
+     {id: "s4", src:"./images/Slide04.jpg"},
+     {id: "s5", src:"./images/Slide05.jpg"},
+     {id: "s6", src:"./images/Slide06.jpg"},
+     {id: "s7", src:"./images/Slide07.jpg"},
+     {id: "s8", src:"./images/Slide08.jpg"},
+     {id: "s9", src:"./images/Slide09.jpg"},
+     {id: "s10", src:"./images/Slide10.jpg"},
+     {id: "s11", src:"./images/Slide11.jpg"},
+     {id: "s12", src:"./images/Slide12.jpg"},
+  ])
+}
+
+function genStims() {
+  stims = {
+    "s1": new createjs.Bitmap(Q.getResult("s1")),
+    "s2": new createjs.Bitmap(Q.getResult("s2")),
+    "s3": new createjs.Bitmap(Q.getResult("s3")),
+    "s4": new createjs.Bitmap(Q.getResult("s4")),
+    "s5": new createjs.Bitmap(Q.getResult("s5")),
+    "s6": new createjs.Bitmap(Q.getResult("s6")),
+    "s7": new createjs.Bitmap(Q.getResult("s7")),
+    "s8": new createjs.Bitmap(Q.getResult("s8")),
+    "s9": new createjs.Bitmap(Q.getResult("s9")),
+    "s10": new createjs.Bitmap(Q.getResult("s10")),
+    "s11": new createjs.Bitmap(Q.getResult("s11")),
+    "s12": new createjs.Bitmap(Q.getResult("s12")),
+  }
+
+  Object.keys(stims).forEach(function(key){
+    var img = stims[ key ]
+    img.regY = img.image.naturalHeight / 2
+    img.regX = img.image.naturalWidth / 2
+    img.y = canvas.height / 2
+    img.x = canvas.width / 2
+  })
+  
+  var symbols = ["A","B","C","D","E","F","G","H","I","X","Y","Z"]
+  var stim_keys = Object.keys(stims)
+  randomize(symbols)
+  mapsym = {}
+  var i = 0
+  while(i < 12){
+    mapsym[ symbols[i] ] = stims[ stim_keys[i] ]
+    i += 1
+  }
+  
+  startTicker()
+}
+
+function startTicker(){
+  createjs.Ticker.addEventListener("tick", runTask)
+  createjs.Ticker.framerate = 1000
+  startTrain()
+}
+
+function startTrain(){
+  stage.addChild(mapsym[fwd_train[0]])
+  stage.update()
+  stim_start = getMS()
+  mode = 'train'
+}
+
+function startTest(){
+  stim_index = 0
+  stage.removeAllChildren()
+  stage.addChild(mapsym[fwd_train[stim_index][test_index]])
+  stage.update()
+  stim_start = getMS()
+  mode = 'test'
+}
+
+function runTask(event){
+  if(mode == 'train'){
+    if(stim_index < train_end_index){
+      if( (getMS() - stim_start) > stim_delay) {
+        if(needPause == 1) {stage.removeAllChildren(), stage.update(), mode = 'train_pause', pause_start = getMS()}
+        else {
+          //stage.removeAllChildren()
+          stim_index += 1
+          stage.addChild(mapsym[ fwd_train[stim_index] ])
+          stage.update()
+          console.log(getMS() - stim_start)
+          stim_start = getMS()
+          needPause = 1
+        }
+      }
+    }
+    else{ 
+      startTest()
+    }
+  }
+  
+  
+  
+  if(mode == 'test'){
+    if(stim_index < test_end_index){
+      if( (getMS() - stim_start) > stim_delay) {
+        stage.removeAllChildren()
+        test_index += 1
+        if(test_index == 2 && needPause == 1){ 
+          mode = 'pause'
+          pause_start = getMS()
+          needPause = 0
+          test_index -= 1 
+        }
+        else if(test_index > 3){stim_index += 1; test_index = -1; needPause = 1;  mode = "ask"}
+        else{
+          stage.addChild(mapsym[ fwd_test[stim_index][test_index] ])
+          stim_start = getMS()
+        }
+        stage.update()
+      }
+    }
+  }
+  
+  if(mode == 'train_pause'){
+    if( (getMS() - pause_start) > train_pause_delay) {
+      needPause = 0
+      mode = 'train'
+    }
+  }
+  
+  if(mode == 'test_pause'){
+    if( (getMS() - pause_start) > test_pause_delay) {
+      mode = 'test'
+    }
+  }
+  
+  if(mode == 'ask'){
+    var text = new createjs.Text("Which pair of symbols was most like what you saw during training?\n\nPress 1 for the first, or 2 for the second.", font_info)
+    var ypos = stage.canvas.height / 2
+    text.x = 100
+    text.y = ypos - 100
+    stage.addChild(text)
+    stage.update()
+  }
+}
+
+document.addEventListener("keydown", handleKey)
+function handleKey(event){
+  //console.log(event)
+  if(mode == 'ask'){
+    if(event.key == "1" || event.key == "2"){
+      var symbols = fwd_test[ (stim_index) - 1]
+      dataSet.push( {"symbols": symbols, "choice": event.key} )
+      mode = 'test'
+    }
+  }
+}
